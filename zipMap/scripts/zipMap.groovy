60fcfc8906d9d2163292ad99be7b7510dbf4f892
boercher <>
2011-12-08 08:28:04 +0100

    initial version

---
+++ zipMap/scripts/zipMap.groovy
@@ -0,0 +1,221 @@
+// @ExecutionModes({ON_SINGLE_NODE})
+// Copyright (C) 2011 Volker Boerchers
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+
+import java.io.BufferedWriter
+import java.io.File
+import java.net.URI
+import java.text.MessageFormat
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+
+import javax.swing.JFileChooser
+import javax.swing.JOptionPane
+
+import org.freeplane.core.ui.ExampleFileFilter
+import org.freeplane.core.util.FreeplaneVersion
+import org.freeplane.core.util.LogUtils
+import org.freeplane.features.map.MapModel
+import org.freeplane.features.map.MapWriter.Mode
+import org.freeplane.features.mode.Controller
+import org.freeplane.features.mode.ModeController
+import org.freeplane.features.mode.mindmapmode.MModeController
+import org.freeplane.features.url.mindmapmode.MFileManager
+import org.freeplane.plugin.script.proxy.MapProxy
+import org.freeplane.plugin.script.proxy.Proxy
+
+
+private MapModel loadMap(URL url) {
+    final ModeController modeController = Controller.getCurrentController().getModeController(MModeController.MODENAME)
+    final MFileManager fileManager = (MFileManager) MFileManager.getController(modeController)
+    MapModel newMap = modeController.getMapController().newModel(null);
+    if (c.freeplaneVersion.isOlderThan(FreeplaneVersion.getVersion('1.2.12'))) {
+        // ugly - no check possible
+        fileManager.loadImpl(url, newMap)
+    }
+    else {
+        if (!fileManager.loadImpl(url, newMap)) {
+            return null
+        }
+    }
+    return newMap
+}
+
+private byte[] getZipBytes(Map<File, String> fileToPathInZipMap, File mapFile, byte[] mapBytes) {
+    def byteArrayOutputStream = new ByteArrayOutputStream()
+    ZipOutputStream zipOutput = new ZipOutputStream(byteArrayOutputStream);
+    fileToPathInZipMap.each{ file, path ->
+        zipOutput = addZipEntry(zipOutput, file, path)
+    }
+    logger.info("zipMap: added ${mapFile.name}")
+    ZipEntry entry = new ZipEntry(mapFile.name)
+    entry.time = mapFile.lastModified()
+    zipOutput.putNextEntry(entry)
+    zipOutput << mapBytes
+    zipOutput.close()
+    return byteArrayOutputStream.toByteArray()
+}
+
+private ZipOutputStream addZipEntry(ZipOutputStream zipOutput, File file, String path) {
+	if (file.isDirectory() && !path.endsWith('/')){
+		path += "/"
+	}
+	logger.info("zipMap: added $path")
+	ZipEntry entry = new ZipEntry(path)
+	entry.time = file.lastModified()
+	zipOutput.putNextEntry(entry)
+	if (file.isFile()) {
+        def fileInputStream = new FileInputStream(file)
+		zipOutput << fileInputStream
+        fileInputStream.close()
+	}
+	return zipOutput
+}
+
+private String getPathInZip(File file, String dependenciesDir, Map<File, String> fileToPathInZipMap) {
+    def mappedPath = fileToPathInZipMap[file]
+    if (mappedPath)
+        return mappedPath
+    def path = "${dependenciesDir}/${file.name}"
+    if (file.isDirectory())
+        path += '/'
+    // TODO: include the parent's name in the path for duplicates
+    while (contains(fileToPathInZipMap.values(), path)) {
+        // if multiple file with the same name (but different directories) are referenced append something to the path
+        path = path.replaceFirst('(\\.\\w+)?$', '1$1')
+        logger.info("zipMap: mapped $file to $path")
+    }
+    return path
+}
+
+def boolean contains(Collection collection, String path) {
+    return collection.contains(path)
+}
+
+private byte[] getBytes(MapModel map) {
+    StringWriter stringWriter = new StringWriter(4*1024)
+    BufferedWriter out = new BufferedWriter(stringWriter)
+    Controller.getCurrentModeController().getMapController().getMapWriter()
+        .writeMapAsXml(map, out, Mode.FILE, true, false)
+    return stringWriter.buffer.toString().bytes
+}
+
+private boolean confirmOverwrite(File file) {
+    def title = getText('Create zip file')
+    def question = textUtils.format('file_already_exists', file);
+    int selection = JOptionPane.showConfirmDialog(ui.frame, question, title, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+    return selection == JOptionPane.YES_OPTION
+}
+
+private File askForZipFile(File zipFile) {
+    def zipFileFilter = new ExampleFileFilter('zip')
+    def chooser = new JFileChooser(fileSelectionMode:JFileChooser.FILES_ONLY, fileFilter:zipFileFilter, selectedFile:zipFile)
+    if (chooser.showOpenDialog() == JFileChooser.APPROVE_OPTION) {
+        if (!chooser.selectedFile.exists() || confirmOverwrite(chooser.selectedFile))
+            return chooser.selectedFile
+    }
+    return null
+}
+
+public File getUriAsFile(File mapDir, URI uri) {
+    try {
+        if (uri == null)
+            return null;
+        def scheme = uri.getScheme()
+        if (!uri.isAbsolute() && (scheme == null || scheme.equals("file"))) {
+            return new File(mapDir, uri.getPath());
+        }
+        return new File(uri);
+    }
+    catch (Exception e) {
+        LogUtils.info("link is not a file uri: " + e);
+        return null;
+    }
+}
+
+private createFileToPathInZipMap(MapModel newMap, String dependenciesDir) {
+    File mapDir = node.map.file.parentFile
+    def fileToPathInZipMap = new MapProxy(newMap, null).root.findAll().inject([:]){ map, node ->
+        def path
+        // == link
+        path = getMappedPath(node.link.uri, map, mapDir, dependenciesDir)
+        if (path)
+            node.link.text = path
+        // == external object
+        path = getMappedPath(node.externalObject.uri, map, mapDir, dependenciesDir)
+        if (path)
+            node.externalObject.uri = path
+        // == attributes
+        def attributes = node.attributes
+        for (int i = 0; i < attributes.size(); i++) {
+            def value = attributes.get(i)
+            if (value instanceof URI) {
+                path = getMappedPath(value, map, mapDir, dependenciesDir)
+                if (path)
+                    attributes.set(i, new URI(path))
+            }
+        }
+        return map
+    }
+    return fileToPathInZipMap
+}
+
+private String getMappedPath(Object uriObject, Map<File, String> fileToPathInZipMap, File mapDir, String dependenciesDir) {
+    if (!uriObject)
+        return null
+    URI uri = (uriObject instanceof URI) ? uriObject : new URI(uriObject.toString())
+    def f = getUriAsFile(mapDir, uri)
+    if (f != null && f.exists()) {
+        def path = getPathInZip(f, dependenciesDir, fileToPathInZipMap)
+        fileToPathInZipMap[f] = path
+        return path
+    }
+    return null
+}
+
+private String getText(String key, Object... parameters) {
+    if (parameters)
+        return MessageFormat.format(key, parameters)
+    return key
+}
+
+boolean zipMap(File file) {
+    if (file == null) {
+        ui.errorMessage(getText('You have to save this map first'))
+        return
+    }
+    if (!node.map.isSaved()) {
+        def question = getText('Do you want to save {0} first?', node.map.name)
+        def title = getText('Create zip file')
+        final int selection = JOptionPane.showConfirmDialog(ui.frame, question, title, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+        if (selection == JOptionPane.YES_OPTION)
+            node.map.save(false)
+        else if (selection == JOptionPane.CANCEL_OPTION)
+            return
+    }
+    def baseName = file.name.replaceFirst('\\.mm', '')
+    def zipFile = askForZipFile(new File(file.parentFile, baseName + '.zip'))
+    if (zipFile == null)
+        return
+    def dependenciesDir = "${baseName}-files"
+    MapModel newMap = loadMap(file.toURI().toURL())
+    if (newMap == null) {
+        ui.errorMessage(getText('Can not create a copy of {0}', file))
+        return
+    }
+    // original file -> zip file name
+    def fileToPathInZipMap = createFileToPathInZipMap(newMap, dependenciesDir)
+    // add the map itself
+    def bytes = getZipBytes(fileToPathInZipMap, file, getBytes(newMap))
+    
+    zipFile.bytes = bytes
+    logger.info("zipMap: wrote ${zipFile.absolutePath}")
+    c.statusInfo = getText('wrote {0}', zipFile.absolutePath)
+}
+
+///////////////////// MAIN //////////////////////////
+zipMap(node.map.file)
---
+++ zipMap/test/zipMap.mm
@@ -0,0 +1,118 @@
+<map version="0.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="test zipMap.groovy" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1323325905264"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork" MAX_WIDTH="600">
+<font NAME="SansSerif" SIZE="12" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.ok">
+<icon BUILTIN="button_ok"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.needs_action">
+<icon BUILTIN="messagebox_warning"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.floating_node">
+<cloud COLOR="#ffffff" SHAPE="ARC"/>
+<edge STYLE="hide_edge"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.connection" COLOR="#606060" STYLE="fork">
+<font NAME="Arial" SIZE="10" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" COLOR="#ff0000">
+<font NAME="Liberation Sans" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.question">
+<icon BUILTIN="help"/>
+<font NAME="Aharoni" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.key" COLOR="#996600">
+<icon BUILTIN="password"/>
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.idea">
+<icon BUILTIN="idea"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.note" COLOR="#990000">
+<font NAME="Liberation Sans" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.date" COLOR="#0033ff">
+<icon BUILTIN="calendar"/>
+<font NAME="Liberation Sans" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.website" COLOR="#006633">
+<font NAME="Liberation Sans" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.list" COLOR="#cc6600">
+<icon BUILTIN="list"/>
+<font NAME="Liberation Sans" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.quotation" COLOR="#338800" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.definition" COLOR="#666600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.description" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.pending" COLOR="#b3b95c">
+<font NAME="Liberation Sans" SIZE="12"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="20"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="12"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode TEXT="red" COLOR="#ff0300"/>
+<stylenode TEXT="green" COLOR="#00cd00"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<attribute NAME="aFile" VALUE="files/afile" OBJECT="java.net.URI|files/afile"/>
+<attribute NAME="aPage" VALUE="files/afile.html" OBJECT="java.net.URI|files/afile.html"/>
+<node TEXT="a.html" POSITION="right" ID="ID_1312236021" CREATED="1323189964670" MODIFIED="1323190058849" LINK="a/tt.html"/>
+<node TEXT="b.html" POSITION="right" ID="ID_1611405813" CREATED="1323189967862" MODIFIED="1323190064909" LINK="b/tt.html"/>
+<node TEXT="c.html" POSITION="right" ID="ID_1140016056" CREATED="1323189969096" MODIFIED="1323190068876" LINK="c/tt.html"/>
+<node TEXT="d.html" POSITION="right" ID="ID_778474213" CREATED="1323189970412" MODIFIED="1323190075840" LINK="d/tt.html"/>
+<node TEXT="image" POSITION="right" ID="ID_1655262636" CREATED="1323280432040" MODIFIED="1323280510494">
+<hook URI="img/image.png" SIZE="0.8955224" NAME="ExternalObject"/>
+</node>
+<node TEXT="a" POSITION="left" ID="ID_1610065305" CREATED="1323189964670" MODIFIED="1323189990517" LINK="a/tt"/>
+<node TEXT="b" POSITION="left" ID="ID_1730188677" CREATED="1323189967862" MODIFIED="1323190001031" LINK="b/tt"/>
+<node TEXT="c" POSITION="left" ID="ID_331522132" CREATED="1323189969096" MODIFIED="1323190009984" LINK="c/tt"/>
+<node TEXT="d" POSITION="left" ID="ID_184659120" CREATED="1323189970412" MODIFIED="1323190020929" LINK="d/tt"/>
+</node>
+</map>
